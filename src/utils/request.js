fee2dc29507ee494246373dfabf07be775053307
Ogabek Yusupov <ogabekyusupov@gmail.com>
2019-11-13 19:00:56 +0500

    Change api url

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getToken } from './token';
 const service = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://delivery-service-back.herokuapp.com/',
   timeout: 2000
 });
 
